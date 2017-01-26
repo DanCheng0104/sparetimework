d07b7ecac9f0db5ce5794e753643cfe3c59631e1
dcheng0104 <dcheng0104@gmail.com>
2017-01-26 09:24:43 -0800

    local server set up, basic setup for html, js and css

--- public/script.js
+++ public/script.js
@@ -1,11 +1,37 @@
-          function initMap() {
-            var uluru = {lat: -25.363, lng: 131.044};
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 4,
-              center: uluru
-            });
-            var marker = new google.maps.Marker({
-              position: uluru,
-              map: map
-            });
-          }
+
+mapInit();
+
+
+function mapInit() {
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGNoZW5nMDEwNCIsImEiOiJjaXE0MDh2MHQwMG9xZnhtNGg0azVybGxtIn0.7jdNnbpd8kQI3qO1HfSnUg';
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    zoom: 8,
+    center: [-118.6919112, 34.0201812]
+});
+
+// const map = L.mapbox.map('map', 'mapbox.streets')
+//     .setView([34.0201812,-118.6919112,10], 9);
+
+map.on('load', function () {
+    map.addLayer({
+        "id": "tracts",
+        "type": "line",
+        "source": {
+            type: 'vector',
+            url: 'mapbox://dcheng0104.bxyckrlg'
+        },
+        "source-layer":"census_tracts-1v46gg",
+        "paint": {
+                "line-color": "#3887be"
+            }
+
+    });
+});
+
+
+
+}
